96958cfead6094a65f0115696c57dfa7a63d0275
Suvam Ligal <suvamligal@gmail.com>
2022-06-23 22:39:43 -0500

    first commit

---
+++ src/main/java/com/lashoo/LashooApplication.java
@@ -0,0 +1,13 @@
+package com.lashoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LashooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LashooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lashoo/LashooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lashoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LashooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
