59b6eeb2fa3c86e179db9953da7c97f4a2ed5a0a
suvamligal <suvamligal@gmail.com>
2023-02-22 22:19:42 -0600

    added lombok jar and updated classes accordingly

--- src/main/java/com/lashoo/model/Employees.java
+++ src/main/java/com/lashoo/model/Employees.java
@@ -1,6 +1,16 @@
 package com.lashoo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Employees {
 
 	private int employeeId;
@@ -10,72 +20,5 @@ public class Employees {
 	private String email;
 	private String username;
 	private String password;
-	
-	public Employees() {
-		
-	}
-	
-	public Employees(int employeeId, String firstName, String lastName, String phoneNumber, String email,
-			String username, String password) {
-		super();
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-		this.username = username;
-		this.password = password;
-	}
-	
-	public int getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
-	@Override
-	public String toString() {
-		return "Employees [employeeId=" + employeeId + ", firstName=" + firstName + ", lastName=" + lastName
-				+ ", phoneNumber=" + phoneNumber + ", email=" + email + ", username=" + username + ", password="
-				+ password + "]";
-	}
-	
-	
 }
